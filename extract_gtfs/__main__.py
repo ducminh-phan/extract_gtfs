e3e1097318ce681180760b7674ac4281f3b66611
Duc-Minh Phan <alephvn@gmail.com>
2018-05-26 13:59:12 +0200

    Reorganize functions

--- extract_gtfs/__main__.py
+++ extract_gtfs/__main__.py
@@ -1,31 +1,6 @@
-import argparse
-
 from .data import setup
 from .extract_dates import CalendarDates
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Extract information from GTFS files "
-                                                 "to use with RAPTOR algorithm")
-
-    parser.add_argument("folder", help="The folder containing the GTFS files to extract")
-    parser.add_argument("-o", "--output", default=None, help="The name of the output folder. "
-                                                             "The default name is obtained by appending "
-                                                             "'_out' to the input folder name")
-
-    args = parser.parse_args()
-    args = check_args(parser, args)
-
-    return args
-
-
-def check_args(parser, args):
-    if args.output is None:
-        args.output = args.folder + "_out"
-    elif args.output == args.folder:
-        parser.error("The input and output folders' names must be different")
-
-    return args
+from .utils import parse_args
 
 
 def main():
--- extract_gtfs/utils.py
+++ extract_gtfs/utils.py
@@ -1,7 +1,32 @@
+import argparse
 import os
 import pickle
 from functools import wraps
 from time import time
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Extract information from GTFS files "
+                                                 "to use with RAPTOR algorithm")
+
+    parser.add_argument("folder", help="The folder containing the GTFS files to extract")
+    parser.add_argument("-o", "--output", default=None, help="The name of the output folder. "
+                                                             "The default name is obtained by appending "
+                                                             "'_out' to the input folder name")
+
+    args = parser.parse_args()
+    args = check_args(parser, args)
+
+    return args
+
+
+def check_args(parser, args):
+    if args.output is None:
+        args.output = args.folder + "_out"
+    elif args.output == args.folder:
+        parser.error("The input and output folders' names must be different")
+
+    return args
 
 
 def measure_time(func):

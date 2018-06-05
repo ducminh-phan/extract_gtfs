3f83dfb5722af34f27edaf8e6f759114c4440999
Duc-Minh Phan <alephvn@gmail.com>
2018-06-05 14:53:30 +0200

    Change uint64_t to int64_t to avoid overflow

--- merge_graph/close_nodes.cpp
+++ merge_graph/close_nodes.cpp
@@ -13,9 +13,9 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-using coordinates_t = std::pair<uint64_t, uint64_t>;
-using coor_table_t = std::unordered_map<uint64_t, coordinates_t>;
-using node_set_t = std::unordered_set<uint64_t>;
+using coordinates_t = std::pair<int64_t, int64_t>;
+using coor_table_t = std::unordered_map<int64_t, coordinates_t>;
+using node_set_t = std::unordered_set<int64_t>;
 
 
 // Parse the mapping node_id -> (node_lon, node_lat) from co files
@@ -23,7 +23,7 @@ coor_table_t parse_coordinates(const char* path) {
     std::fstream nodes_co {path};
 
     coor_table_t table;
-    for (CSVIterator<uint64_t> iter {&nodes_co, false, ' '}; iter != CSVIterator<uint64_t>(); ++iter) {
+    for (CSVIterator<int64_t> iter {&nodes_co, false, ' '}; iter != CSVIterator<int64_t>(); ++iter) {
         table.insert({(*iter)[1], {(*iter)[2], (*iter)[3]}});
     }
 
@@ -36,7 +36,7 @@ node_set_t parse_nodes(const char* path) {
     std::fstream nodes_gr {path};
 
     node_set_t node_set;
-    for (CSVIterator<uint64_t> iter {&nodes_gr, false, ' '}; iter != CSVIterator<uint64_t>(); ++iter) {
+    for (CSVIterator<int64_t> iter {&nodes_gr, false, ' '}; iter != CSVIterator<int64_t>(); ++iter) {
         node_set.insert((*iter)[1]);
         node_set.insert((*iter)[2]);
     }
@@ -44,19 +44,20 @@ node_set_t parse_nodes(const char* path) {
     return node_set;
 }
 
-
+// Approximate the great-circle distance given the coordinates of two points.
+// Reference: https://www.movable-type.co.uk/scripts/latlong.html
 double distance(coordinates_t p1, coordinates_t p2) {
-    static const double R {6371000.0};
-    auto x1 = p1.first;
-    auto x2 = p2.first;
-    auto y1 = p1.second;
-    auto y2 = p2.second;
+    static const double R {63710000};
+    auto lon_1 = p1.first;
+    auto lon_2 = p2.first;
+    auto lat_1 = p1.second;
+    auto lat_2 = p2.second;
 
-    double phi_m = (y1 + y2) / 2 / 1000000.0;
-    double x = (x2 - x1) * cos(M_PI * phi_m / 180);
-    double y = y2 - y1;
+    double phi_m = (lat_1 + lat_2) / 2e6;
+    double x = (lon_2 - lon_1) * cos(M_PI * phi_m / 180);
+    double y = lat_2 - lat_1;
 
-    double res = sqrt(x * x + y * y) * R * M_PI / 180 / 1000000.0;
+    double res = sqrt(x * x + y * y) * R * M_PI / 180 / 1e6;
     return res;
 }
 
